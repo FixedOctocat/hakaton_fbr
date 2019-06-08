5b65cdc344133201031143c048b0a7af5b00c5eb
maskaa <51506154+maskaa@users.noreply.github.com>
2019-06-08 12:33:07 +0300

    Create face_compare.py

---
+++ face_compare.py
@@ -0,0 +1,12 @@
+import face_recognition
+
+def face_compare(img1, img2):
+    face = face_recognition.load_image_file(img1)
+    face = face_recognition.face_encodings(face)[0]
+    unknown_picture = face_recognition.load_image_file(img2)
+    unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+    results = face_recognition.compare_faces([face], unknown_face_encoding)
+    if results[0] == True:
+        print( "Success!")
+    else:
+        print("Failed")
