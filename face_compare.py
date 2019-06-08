95f5e5974da58ce305e648507b4b9646292e7ff2
maskaa <51506154+maskaa@users.noreply.github.com>
2019-06-08 16:48:05 +0300

    Update face_compare.py

--- face_compare.py
+++ face_compare.py
@@ -1,3 +1,4 @@
+import glob
 import face_recognition
 
 def face_compare_func(img1, img2):
@@ -6,7 +7,13 @@ def face_compare_func(img1, img2):
     unknown_picture = face_recognition.load_image_file(img2)
     unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
     results = face_recognition.compare_faces([face], unknown_face_encoding)
+    
     if results[0] == True:
-        return True
+        return img1
     else:
         return False
+
+def open_files(img):
+    for filename in glob.glob('*.jpg'):
+        f = open(filename)
+        face_compare_func(f, img)
