d7bb870b8db8194699cbd6f719c2260fa4bd9c20
FixedOctocat <43385721+FixedOctocat@users.noreply.github.com>
2019-06-08 14:58:31 +0300

    Update url_getting.py

--- url_getting.py
+++ url_getting.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-def url_getting(stroka_egora):
-    url = "https://vk.com/photo" + str(stroka_egora) + "?rev=1"
+def url_getting_func(str):
+    url = "https://vk.com/photo" + str(str) + "?rev=1"
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     a = soup.find('img')
